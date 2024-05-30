93afe23ff433b1857aa463a0136bf53a1d419fb3
Anthony Polka <anthonypolka@gmail.com>
2024-05-30 12:57:38 -0500

    lookup table

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -23,13 +23,13 @@ C	Carry
 namespace
 {
     /* flags */
-    [[maybe_unused]] constexpr std::uint8_t C = 1 << 0; // carry
-    [[maybe_unused]] constexpr std::uint8_t Z = 1 << 1; // zero
-    [[maybe_unused]] constexpr std::uint8_t I = 1 << 2; // interrupt
-    [[maybe_unused]] constexpr std::uint8_t D = 1 << 3; // decimal
-    [[maybe_unused]] constexpr std::uint8_t B = 1 << 4; // break
-    [[maybe_unused]] constexpr std::uint8_t V = 1 << 6; // overflow
-    [[maybe_unused]] constexpr std::uint8_t N = 1 << 7; // negative
+    [[maybe_unused]] constexpr std::uint8_t C = 1 << 0;
+    [[maybe_unused]] constexpr std::uint8_t Z = 1 << 1;
+    [[maybe_unused]] constexpr std::uint8_t I = 1 << 2;
+    [[maybe_unused]] constexpr std::uint8_t D = 1 << 3;
+    [[maybe_unused]] constexpr std::uint8_t B = 1 << 4;
+    [[maybe_unused]] constexpr std::uint8_t V = 1 << 6;
+    [[maybe_unused]] constexpr std::uint8_t N = 1 << 7;
 
 }
 
@@ -48,36 +48,40 @@ void _6502::load_rom(const char* filePath)
     fclose (file);
 }
 
+_6502::_6502()
+{
+    opcodes =
+    {{
+        {"BRK", &_6502::BRK, MODE::IMP, 7}, {"ORA", &_6502::ORA, MODE::IZX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"ORA", &_6502::ORA, MODE::ZPG, 3}, {"ASL", &_6502::ASL, MODE::ZPG, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"PHP", &_6502::PHP, MODE::IMP, 3}, {"ORA", &_6502::ORA, MODE::IMM, 2}, {"ASL", &_6502::ASL, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"ORA", &_6502::ORA, MODE::ABS, 4}, {"ASL", &_6502::ASL, MODE::ABS, 6}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"BPL", &_6502::BPL, MODE::REL, 2}, {"ORA", &_6502::ORA, MODE::IZY, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"ORA", &_6502::ORA, MODE::ZPX, 3}, {"ASL", &_6502::ASL, MODE::ZPX, 7}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CLC", &_6502::CLC, MODE::IMP, 2}, {"ORA", &_6502::ORA, MODE::ABY, 4}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"ORA", &_6502::ORA, MODE::ABX, 4}, {"ASL", &_6502::ASL, MODE::ABX, 7}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"JSR", &_6502::JSR, MODE::ABS, 6}, {"AND", &_6502::AND, MODE::IZX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"BIT", &_6502::BIT, MODE::ZPG, 3}, {"AND", &_6502::AND, MODE::ZPG, 3}, {"ROL", &_6502::ROL, MODE::ZPG, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"PLP", &_6502::PLP, MODE::IMP, 4}, {"AND", &_6502::AND, MODE::IMM, 2}, {"ROL", &_6502::ROL, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"BIT", &_6502::BIT, MODE::ABS, 4}, {"AND", &_6502::AND, MODE::ABS, 4}, {"ROL", &_6502::ROL, MODE::ABS, 6}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"BMI", &_6502::BMI, MODE::REL, 2}, {"AND", &_6502::AND, MODE::IZY, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"AND", &_6502::AND, MODE::ZPX, 4}, {"ROL", &_6502::ROL, MODE::ZPX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"SEC", &_6502::SEC, MODE::IMP, 2}, {"AND", &_6502::AND, MODE::ABY, 4}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"AND", &_6502::AND, MODE::ABX, 4}, {"ROL", &_6502::ROL, MODE::ABX, 7}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"RTI", &_6502::RTI, MODE::IMP, 6}, {"EOR", &_6502::EOR, MODE::IZX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"EOR", &_6502::EOR, MODE::ZPG, 3}, {"LSR", &_6502::LSR, MODE::ZPG, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"PHA", &_6502::PHA, MODE::IMP, 3}, {"EOR", &_6502::EOR, MODE::IMM, 2}, {"LSR", &_6502::LSR, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"JMP", &_6502::JMP, MODE::ABS, 3}, {"EOR", &_6502::EOR, MODE::ABS, 4}, {"LSR", &_6502::LSR, MODE::ABS, 6}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"BVC", &_6502::BVC, MODE::REL, 2}, {"EOR", &_6502::EOR, MODE::IZY, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"EOR", &_6502::EOR, MODE::ZPX, 4}, {"LSR", &_6502::LSR, MODE::ZPX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CLI", &_6502::CLI, MODE::IMP, 2}, {"EOR", &_6502::EOR, MODE::ABY, 4}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"EOR", &_6502::EOR, MODE::ABX, 4}, {"LSR", &_6502::LSR, MODE::ABX, 7}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"RTS", &_6502::RTS, MODE::IMP, 6}, {"ADC", &_6502::ADC, MODE::IZX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"ADC", &_6502::ADC, MODE::ZPG, 3}, {"ROR", &_6502::ROR, MODE::ZPG, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"PLA", &_6502::PLA, MODE::IMP, 4}, {"ADC", &_6502::ADC, MODE::IMM, 2}, {"ROR", &_6502::ROR, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"JMP", &_6502::JMP, MODE::IND, 5}, {"ADC", &_6502::ADC, MODE::ABS, 4}, {"ROR", &_6502::ROR, MODE::ABS, 6}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"BVS", &_6502::BVS, MODE::REL, 2}, {"ADC", &_6502::ADC, MODE::IZY, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"ADC", &_6502::ADC, MODE::ZPX, 4}, {"ROR", &_6502::ROR, MODE::ZPX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"SEI", &_6502::SEI, MODE::IMP, 2}, {"ADC", &_6502::ADC, MODE::ABY, 4}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"ADC", &_6502::ADC, MODE::ABX, 4}, {"ROR", &_6502::ROR, MODE::ABX, 7}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"???", &_6502::XXX, MODE::IMM, 0}, {"STA", &_6502::STA, MODE::IZX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"STY", &_6502::STY, MODE::ZPG, 3}, {"STA", &_6502::STA, MODE::ZPG, 3}, {"STX", &_6502::STX, MODE::ZPG, 3}, {"???", &_6502::XXX, MODE::IMM, 0}, {"DEY", &_6502::DEY, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"TXA", &_6502::TXA, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"STY", &_6502::STY, MODE::ABS, 4}, {"STA", &_6502::STA, MODE::ABS, 4}, {"STX", &_6502::STX, MODE::ABS, 4}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"BBC", &_6502::BBC, MODE::REL, 2}, {"STA", &_6502::STA, MODE::IZY, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"STY", &_6502::STY, MODE::ZPX, 4}, {"STA", &_6502::STA, MODE::ZPX, 4}, {"STX", &_6502::STX, MODE::ZPY, 4}, {"???", &_6502::XXX, MODE::IMM, 0}, {"TYA", &_6502::TYA, MODE::IMP, 2}, {"STA", &_6502::STA, MODE::ABY, 5}, {"TXS", &_6502::TXS, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"STA", &_6502::STA, MODE::ABX, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"LDY", &_6502::LDY, MODE::IMM, 2}, {"LDA", &_6502::LDA, MODE::IZX, 6}, {"LDX", &_6502::LDX, MODE::IMM, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"LDY", &_6502::LDY, MODE::ZPG, 3}, {"LDA", &_6502::LDA, MODE::ZPG, 3}, {"LDX", &_6502::LDX, MODE::ZPG, 3}, {"???", &_6502::XXX, MODE::IMM, 0}, {"TAY", &_6502::TAY, MODE::IMP, 2}, {"LDA", &_6502::LDA, MODE::IMM, 2}, {"TAX", &_6502::TAX, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"LDY", &_6502::LDY, MODE::ABS, 4}, {"LDA", &_6502::LDA, MODE::ABS, 4}, {"LDX", &_6502::LDX, MODE::ABS, 4}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"BCS", &_6502::BCS, MODE::REL, 2}, {"LDA", &_6502::LDA, MODE::IZY, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"LDY", &_6502::LDY, MODE::ZPX, 4}, {"LDA", &_6502::LDA, MODE::ZPX, 4}, {"LDX", &_6502::LDX, MODE::ZPY, 4}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CLV", &_6502::CLV, MODE::IMP, 2}, {"LDA", &_6502::LDA, MODE::ABY, 4}, {"TSX", &_6502::TSX, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"LDY", &_6502::LDY, MODE::ABX, 4}, {"LDA", &_6502::LDA, MODE::ABX, 4}, {"LDX", &_6502::LDX, MODE::ABY, 4}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"CPY", &_6502::CPY, MODE::IMM, 2}, {"CMP", &_6502::CMP, MODE::IZX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CPY", &_6502::CPY, MODE::ZPG, 3}, {"CMP", &_6502::CMP, MODE::ZPG, 3}, {"DEC", &_6502::DEC, MODE::ZPG, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"INY", &_6502::INY, MODE::IMP, 2}, {"CMP", &_6502::CMP, MODE::IMM, 2}, {"DEX", &_6502::DEX, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CPY", &_6502::CPY, MODE::ABS, 4}, {"CMP", &_6502::CMP, MODE::ABS, 4}, {"DEC", &_6502::DEC, MODE::ABS, 6}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"BNE", &_6502::BNE, MODE::REL, 2}, {"CMP", &_6502::CMP, MODE::IZY, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CMP", &_6502::CMP, MODE::ZPX, 4}, {"DEC", &_6502::DEC, MODE::ZPX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CLD", &_6502::CLD, MODE::IMP, 2}, {"CMP", &_6502::CMP, MODE::ABY, 4}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CMP", &_6502::CMP, MODE::ABX, 4}, {"DEC", &_6502::DEC, MODE::ABX, 7}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"CPX", &_6502::CPX, MODE::IMM, 2}, {"SBC", &_6502::SBC, MODE::IZX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CPX", &_6502::CPX, MODE::ZPG, 3}, {"SBC", &_6502::SBC, MODE::ZPG, 3}, {"INC", &_6502::INC, MODE::ZPG, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"INX", &_6502::INX, MODE::IMP, 2}, {"SBC", &_6502::SPC, MODE::IMM, 2}, {"NOP", &_6502::NOP, MODE::IMP, 2}, {"???", &_6502::XXX, MODE::IMM, 0}, {"CPX", &_6502::CPX, MODE::ABS, 4}, {"SBC", &_6502::SBC, MODE::ABS, 4}, {"INC", &_6502::INC, MODE::ABS, 6}, {"???", &_6502::XXX, MODE::IMM, 0},
+        {"BEQ", &_6502::BEQ, MODE::REL, 2}, {"SBC", &_6502::SBC, MODE::IZY, 5}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"SBC", &_6502::SBC, MODE::ZPX, 4}, {"INC", &_6502::INC, MODE::ZPX, 6}, {"???", &_6502::XXX, MODE::IMM, 0}, {"SED", &_6502::SED, MODE::IMP, 2}, {"SBC", &_6502::SBC, MODE::ABY, 4}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"???", &_6502::XXX, MODE::IMM, 0}, {"SBC", &_6502::SBC, MODE::ABX, 4}, {"INC", &_6502::INC, MODE::ABX, 7}, {"???", &_6502::XXX, MODE::IMM, 0}
+    }};
 
-// void _6502::BRK(void * val)
-// {
-//     const char* str = (const char*) val;
-//     std::cout << str;
-// }
-
-
-/*
-addressing	assembler	opc	bytes	cycles
-immediate	ADC #oper	69	2	2  
-zeropage	ADC oper	65	2	3  
-zeropage,X	ADC oper,X	75	2	4  
-absolute	ADC oper	6D	3	4  
-absolute,X	ADC oper,X	7D	3	4* 
-absolute,Y	ADC oper,Y	79	3	4* 
-(indirect,X)	ADC (oper,X)	61	2	6  
-(indirect),Y	ADC (oper),Y	71	2	5* 
-
-*/
-
-#define PRINT (str) {std::cout << str << std::endl;}
-
-
+}
 
 
 void _6502::decompiler()
 {
-    opTable[0x00]((void*)"hello world!\n");
+    // (this->*opcodes[0x00].opcode)();
+    // return;
 
+    for (std::size_t i = 0; i < opcodes.size(); i++)
+    {
+        std::cout << std::format ("{:X} {:s}\n", i, opcodes[i].mnemonic);
+    }
     return;
 
     std::size_t i = 0;
@@ -100,7 +104,62 @@ void _6502::decompiler()
         }
     }
 }
-
-
-
-
+    void _6502::XXX(void){}
+    void _6502::BRK(void){}
+    void _6502::ORA(void){}
+    void _6502::ASL(void){}
+    void _6502::PHP(void){}
+    void _6502::BPL(void){}
+    void _6502::CLC(void){}
+    void _6502::JSR(void){}
+    void _6502::AND(void){}
+    void _6502::BIT(void){}
+    void _6502::ROL(void){}
+    void _6502::PLP(void){}
+    void _6502::BMI(void){}
+    void _6502::SEC(void){}
+    void _6502::RTI(void){}
+    void _6502::EOR(void){}
+    void _6502::LSR(void){}
+    void _6502::PHA(void){}
+    void _6502::JMP(void){}
+    void _6502::BVC(void){}
+    void _6502::CLI(void){}
+    void _6502::RTS(void){}
+    void _6502::PLA(void){}
+    void _6502::ADC(void){}
+    void _6502::ROR(void){}
+    void _6502::BVS(void){}
+    void _6502::SEI(void){}
+    void _6502::STA(void){}
+    void _6502::STY(void){}
+    void _6502::STX(void){}
+    void _6502::DEY(void){}
+    void _6502::TXA(void){}
+    void _6502::STZ(void){}
+    void _6502::BBC(void){}
+    void _6502::TYA(void){}
+    void _6502::TXS(void){}
+    void _6502::LDY(void){}
+    void _6502::LDA(void){}
+    void _6502::LDX(void){}
+    void _6502::TAY(void){}
+    void _6502::TAX(void){}
+    void _6502::BCS(void){}
+    void _6502::CLV(void){}
+    void _6502::TSX(void){}
+    void _6502::CPY(void){}
+    void _6502::CMP(void){}
+    void _6502::DEC(void){}
+    void _6502::INY(void){}
+    void _6502::DEX(void){}
+    void _6502::BNE(void){}
+    void _6502::CLD(void){}
+    void _6502::CPX(void){}
+    void _6502::SBC(void){}
+    void _6502::INC(void){}
+    void _6502::INX(void){}
+    void _6502::SPC(void){}
+    void _6502::NOP(void){}
+    void _6502::BEQ(void){}
+    void _6502::SED(void){}
--- src/cpu.hpp
+++ src/cpu.hpp
@@ -3,24 +3,10 @@
 
 #include <cstdint>
 #include <array>
-#include <string>
 
 
 /*
-    https://en.wikipedia.org/wiki/MOS_Technology_6502
-
-    Like its precursor, the 6800, the 6502 has very few registers. The 6502's registers 
-    include one 8-bit accumulator register (A), two 8-bit index registers (X and Y), 7 
-    processor status flag bits (P; from bit 7 to bit 0 these are the negative (N), overflow (V), 
-    reserved, break (B), decimal (D), interrupt disable (I), zero (Z) and carry (C) flag), an 8-bit stack 
-    pointer (S), and a 16-bit program counter (PC).[75] This compares to a contemporaneous competitor, the Intel 
-    8080, which likewise has one 8-bit accumulator and a 16-bit program counter, but has six more general-purpose 8-bit 
-    registers (which can be combined into three 16-bit pointers), and a larger 16-bit stack pointer
-*/
-
-
-/*
-    https://www.masswerk.at/6502/6502_instruction_set.html
+    https://www.masswerk.at/6502/6502_instruction_set.html#BVS
 
     PC	program counter	(16 bit)
     AC	accumulator	(8 bit)
@@ -45,311 +31,108 @@
     LIFO, top-down, 8 bit range, 0x0100 - 0x01FF
 */
 
-
-
-
+enum class MODE : std::uint8_t
+{
+    IMP, // Implied Addressing
+    IMM, // Immediate Addressing
+    ABS, // Absolute Addressing
+    ZPG, // Zero-Page Addressing
+    ABX, // Indexed Addressing: Absolute,X
+    ABY, // Indexed Addressing: Absolute,Y
+    ZPX, // Indexed Addressing: Zero-Page,X
+    ZPY, // Indexed Addressing: (Zero-Page,Y)
+    IND, // Indirect Addressing
+    IZX, // Indirect Zero-page X-indexed
+    IZY, // Indirect Zero-page Y-indexed
+    PZX, // Pre-Indexed Indirect, "(Zero-Page,X)"
+    PZY, // Post-Indexed Indirect, "(Zero-Page),Y"
+    REL, // Relative Addressing (Conditional Branching)
+};
 
 struct _6502
 {
 
-    // main registers
-    std::uint16_t PC;   // program counter
-    std::uint8_t  AC;   // accumulator
-    std::uint8_t  x;    // x register
-    std::uint8_t  y;    // y register
-    std::uint8_t  SR;   // status register [NV-BDIZC] (aka flags)
-    std::uint8_t  SP;   // stack pointer
-    std::array<std::uint8_t, 65536> memory; // 256 pages of 256 bits each
-    void load_rom(const char* filePath);
-    void decompiler();
-
+    /* main registers */
+    std::uint16_t PC;   /* program counter */
+    std::uint8_t  AC;   /* accumulator */
+    std::uint8_t  x;    /* x register */
+    std::uint8_t  y;    /* y register */
+    std::uint8_t  SR;   /* status register [NV-BDIZC] (aka flags) */
+    std::uint8_t  SP;   /* stack pointer */
+    std::array<std::uint8_t, 65536> memory; /* 256 pages of 256 bits each */
     struct instruction
     {
-        void (_6502::*op)(void);
+        const char* mnemonic;
+        void (_6502::*opcode)(void) = nullptr;
+        MODE addressMode;
+        std::uint8_t cycles;
+
     };
+    std::array<instruction,256> opcodes;
+    void load_rom(const char* filePath);
+    void decompiler();
+    _6502();
 
+    void XXX(void); 
+    void BRK(void);
+    void ORA(void);
+    void ASL(void);
+    void PHP(void);
+    void BPL(void);
+    void CLC(void);
+    void JSR(void);
+    void AND(void);
+    void BIT(void);
+    void ROL(void);
+    void PLP(void);
+    void BMI(void);
+    void SEC(void);
+    void RTI(void);
+    void EOR(void);
+    void LSR(void);
+    void PHA(void);
+    void JMP(void);
+    void BVC(void);
+    void CLI(void);
+    void RTS(void);
+    void PLA(void);
+    void ADC(void);
+    void ROR(void);
+    void BVS(void);
+    void SEI(void);
+    void STA(void);
+    void STY(void);
+    void STX(void);
+    void DEY(void);
+    void TXA(void);
+    void STZ(void);
+    void BBC(void);
+    void TYA(void);
+    void TXS(void);
+    void LDY(void);
+    void LDA(void);
+    void LDX(void);
+    void TAY(void);
+    void TAX(void);
+    void BCS(void);
+    void CLV(void);
+    void TSX(void);
+    void CPY(void);
+    void CMP(void);
+    void DEC(void);
+    void INY(void);
+    void DEX(void);
+    void BNE(void);
+    void CLD(void);
+    void CPX(void);
+    void SBC(void);
+    void INC(void);
+    void INX(void);
+    void SPC(void);
+    void NOP(void);
+    void BEQ(void);
+    void SED(void);
 };
-
-
-struct opcode
-{
-
-};
-
-
-namespace opcodes
-{
-    void XXX(void*);        // no opcode
-
-    void BRK_IMPL(void*);   // 0x00
-    void ORA_X_IND(void*);  // 0x01
-    //
-    //
-    //
-    void ORA_ZPG(void*);    // 0x05
-    void ASL_ZPG(void*);    // 0x06
-    //
-    void PHP_IMPL(void*);   // 0x08
-    void ORA_IMM(void*);    // 0x09
-    void ASL_A(void*);      // 0x0A
-    //
-    //
-    void ORA_ABS(void*);    // 0x0D
-    void ASL_ABS(void*);    // 0x0E
-    //
-    void BPL_REL(void*);    // 0x10
-    void ORA_IND_Y(void*);  // 0x11
-    //
-    //
-    //
-    void ORA_ZPG_X(void*);  // 0x15
-    void ASL_ZPG_X(void*);  // 0x16
-    //
-    void CLC_IMPL(void*);   // 0x18
-    void ORA_ABS_Y(void*);  // 0x19
-    //
-    //
-    //
-    void ORA_ABS_X(void*);  // 0x1D
-    void ASL_ABS_X(void*);  // 0x1E
-    //
-    void JSR_ABS(void*);    // 0x20
-    void AND_X_IND(void*);  // 0x21
-    //
-    //
-    void BIT_ZPG(void*);    // 0x24
-    void AND_ZPG(void*);    // 0x25
-    void ROL_ZPG(void*);    // 0x26
-    //
-    void PLP_IMPL(void*);   // 0x28
-    void AND_IMM(void*);    // 0x29
-    void ROL_A(void*);      // 0x2A
-    //
-    //
-    //
-    void BIT_ABS(void*);    // 0x2C
-    void AND_ABS(void*);    // 0x2D
-    void ROL_ABS(void*);    // 0x2E
-    //
-    void BMI_REL(void*);    // 0x30
-    void AND_IND_Y(void*);  // 0x31
-    //
-    //
-    //
-    void AND_ZPG_X(void*);  // 0x35
-    void ROL_ZPG_X(void*);  // 0x36
-    //
-    void SEC_IMPL(void*);   // 0x38
-    void AND_ABS_Y(void*);  // 0x39
-    //
-    //
-    //
-    void AND_ABS_X(void*);  // 0x3D
-    void ROL_ABS_X(void*);  // 0x3E
-    //
-    void RTI_IMPL(void*);   //0x40
-    void EOR_X_IND(void*);  // 0x41
-    //
-    //
-    //
-    void EOR_ZPG(void*);    // 0x45
-    void LSR_ZPG(void*);    // 0x46
-    //
-    void PHA_IMPL(void*);   // 0x48
-    void EOR_IMM(void*);    // 0x49
-    void LSR_A(void*);      // 0x4A
-    //
-    void JMP_ABS(void*);    // 0x4C
-    void EOR_ABS(void*);    // 0x4D
-    void LSR_ABS(void*);    // 0x4E
-    //
-    void BVC_REL(void*);    // 0x50
-    void EOR_IND_Y(void*);  // 0x51
-    //
-    //
-    //
-    void EOR_ZPG_X(void*);  // 0x55
-    void LSR_ZPG_X(void*);  // 0x56
-    //
-    void CLI_IMPL(void*);   // 0x58
-    void EOR_ABS_Y(void*);  // 0x59
-    //
-    //
-    //
-    void EOR_ABS_X(void*);  // 0x5D
-    void LSR_ABS_X(void*);  // 0x5E
-    //
-    void RTS_IMPL(void*);   // 0x60
-    void ADC_X_IND(void*);  // 0x61
-    //
-    //
-    //
-    void ADC_ZPG(void*);    // 0x65
-    void ROR_ZPG(void*);    // 0x66
-    //
-    void PLA_IMPL(void*);   // 0x68
-    void ADC_IMM(void*);    // 0x69
-    void ROR_A(void*);      // 0x6A
-    //
-    void JMP_IND(void*);    // 0x6C
-    void ADC_ABS(void*);    // 0x6D
-    void ROR_ABS(void*);    // 0x6E
-    //
-    void BVS_REL(void*);    // 0x70
-    void ADC_IND_Y(void*);  // 0x71
-    //
-    //
-    //
-    void ADC_ZPG_X(void*);  // 0x75
-    void ROR_ZPG_X(void*);  // 0x76
-    //
-    void SEI_IMPL(void*);   // 0x78
-    void ADC_ABS_Y(void*);  // 0x79
-    //
-    //
-    //
-    void ADC_ABS_X(void*);  // 0x7D
-    void ROR_ABS_X(void*);  // 0x7E
-    //
-    //
-    void STA_X_IND(void*);  // 0x81
-    //
-    //
-    void STY_ZPG(void*);    // 0x84
-    void STA_ZPG(void*);    // 0x85
-    void STX_ZPG(void*);    // 0x86
-    //
-    void DEY_IMPL(void*);   // 0x88
-    //
-    void TEX_IMPL(void*);   // 0x8A
-    //
-    void STY_ABS(void*);    // 0x8C
-    void STA_ABS(void*);    // 0x8D
-    void STZ_ABS(void*);    // 0x8E
-    //
-    void BBC_REL(void*);    // 0x90
-    void STA_IND_Y(void*);  // 0x91
-    //
-    //
-    void STY_ZPG_X(void*);  // 0x94
-    void STA_ZPG_X(void*);  // 0x95
-    void STX_ZPG_Y(void*);  // 0x96
-    //
-    void TYA_IMPL(void*);   // 0x98
-    void STA_ABS_Y(void*);  // 0x99
-    void TXS_IMPL(void*);   // 0x9A
-    //
-    //
-    void STA_ABS_X(void*);  // 0x9D
-    //
-    //
-    void LDY_IMM(void*);    // 0xA0
-    void LDA_X_IND(void*);  // 0xA1
-    void LDX_IMM(void*);    // 0xA2
-    //
-    void LDY_ZPG(void*);    // 0xA4
-    void LDA_ZPG(void*);    // 0xA5
-    void LDX_ZPG(void*);    // 0xA6
-    //
-    void TAY_IMPL(void*);   // 0xA8
-    void LDA_IMM(void*);    // 0xA9
-    void TAX_IMPL(void*);   // 0xAA
-    //
-    void LDY_ABS(void*);    // 0xAC
-    void LDA_ABS(void*);    // 0xAD
-    void LDX_ABS(void*);    // 0xAE
-    //
-    void BCS_REL(void*);    // 0xB0
-    void LDA_IND_Y(void*);  // 0xB1
-    //
-    //
-    void LDY_ZPG_X(void*);  // 0xB4
-    void LDA_ZPG_X(void*);  // 0xB5
-    void LDX_ZPG_Y(void*);  // 0xB6
-    //
-    void CLV_IMPL(void*);   // 0xB8
-    void LDA_ABS_Y(void*);  // 0xB9
-    void TSX_IMPL(void*);   // 0xBA
-    //
-    void LDY_ABS_X(void*);  // 0xBC
-    void LDA_ABS_X(void*);  // 0xBD
-    void LDX_ABS_Y(void*);  // 0xBE
-    //
-    void CPY_IMM(void*);    // 0xC0
-    void CMP_X_IND(void*);  // 0xC1
-    //
-    //
-    void CPY_ZPG(void*);    // 0xC4
-    void CMP_ZPG(void*);    // 0xC5
-    void DEC_ZPG(void*);    // 0xC6
-    //
-    void INY_IMPL(void*);   // 0xC8
-    void CMP_IMM(void*);    // 0xC9
-    void DEX_IMPL(void*);   // 0xCA
-    //
-    void CPY_ABS(void*);    // 0xCC
-    void CMP_ABS(void*);    // 0xCD
-    void DEC_ABS(void*);    // 0xCE
-    //
-    void BNE_REL(void*);    // 0xD0
-    void CMP_IND_Y(void*);  // 0xD1
-    //
-    //
-    //
-    void CMP_ZPG_X(void*);  // 0xD5
-    void DEC_ZPG_X(void*);  // 0xD6
-    //
-    void CLD_IMPL(void*);   // 0xD8
-    void CMP_ABS_Y(void*);  // 0xD9
-    //
-    //
-    //
-    void CMP_ABS_X(void*);  // 0xDD
-    void DEC_ABS_X(void*);  // 0xDE
-    //
-    void CPX_IMM(void*);    // 0xE0
-    void SBC_X_IND(void*);  // 0xE1
-    //
-    //
-    void CPX_ZPG(void*);    // 0xE4
-    void SBC_ZPG(void*);    // 0xE5
-    void INC_ZPG(void*);    // 0xE6
-    //
-    void INX_IMPL(void*);   // 0xE8
-    void SPC_IMM(void*);    // 0xE9
-    void NOP_IMPL(void*);   // 0xEA
-    //
-    void CPX_ABS(void*);    // 0xEC
-    void SBC_ABS(void*);    // 0xED
-    void INC_ABS(void*);    // 0xEE
-    //
-    void BEQ_REL(void*);    // 0xF0
-    void SBC_IND_Y(void*);  // 0xF1
-    //
-    //
-    //
-    void SBC_ZPG_X(void*);  // 0xF5
-    void INC_ZPG_X(void*);  // 0xF6
-    //
-    void SED_IMPL(void*);   // 0xF8
-    void SBC_ABS_Y(void*);  // 0xF9
-    //
-    //
-    //
-    void SBC_ABS_X(void*);  // 0xFD
-    void INC_ABS_X(void*);  // 0xFE
-    //
-
-
-
-
-
-
-
-
-}
-
-
 
 #endif
 
