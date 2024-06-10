5cde54bd907d549b54015f09cd537a3825d89731
Anthony <anthonypolka@gmail.com>
2024-06-10 15:57:10 -0500

    constexpr RAM_SIZE

--- include/common.hpp
+++ include/common.hpp
@@ -3,19 +3,19 @@
 
 #include <cstdint>
 
-#define RAM_SIZE 65536 // 65kb
-
 using byte = std::uint8_t;
 using word = std::uint16_t;
 
+static constexpr std::size_t RAM_SIZE = 65536; //65kb
 
+static constexpr word ZRO_BEGIN  = 0x0000;
+static constexpr word ZRO_END    = 0x00FF;
 
-[[maybe_unused]] constexpr word ZRO_BEGIN  = 0x0000;
-[[maybe_unused]] constexpr word STK_BEGIN  = 0x0100;
-[[maybe_unused]] constexpr word ROM_BEGIN  = 0xF000;
-[[maybe_unused]] constexpr word ZRO_END    = 0x00FF;
-[[maybe_unused]] constexpr word STK_END    = 0x01FF;
-[[maybe_unused]] constexpr word ROM_END    = 0xFFFA;
+static constexpr word STK_BEGIN  = 0x0100;
+static constexpr word STK_END    = 0x01FF;
+
+static constexpr word ROM_BEGIN  = 0xF000;
+static constexpr word ROM_END    = 0xFFFA;
 
 
 
